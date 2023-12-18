4fd3d5b52f097fff1c6bf8873847606517d5a8b0
Yakup KÖK <yakupkok@192.168.1.78>
2023-12-18 14:47:53 +0300

    noscript changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,8 @@ function About() {
   return (
     <div>
       <h1>About my self </h1>
-      <p>I am a management information systems student who will graduate soon. I am a software developer who is motivated toconstantly improve and learn. I am increasing my skills in web front end and mobile application development, especially by working on projects related to React/React Native.</p>
-      <h2>Areas of Interest:</h2>
+      <p> I am a software developer who is motivated toconstantly improve and learn. I am increasing my skills in web front end and mobile application development, especially by working on projects related to React/React Native.</p>
+      <h2>Services:</h2>
       <li> Mobile App Development</li>
       <li>Web Development</li>
       <h3>Skils: </h3>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ import myImage from '../photos/IMG_7358.jpg'
 
 function Home() {
   return (
-    <div className='Container'>
-      <div>
+    <div className='home'>
+      <div className='image'>
       <img src={myImage} alt="yakup kok" style={{borderRadius: 200, width:400, height:400, marginTop: 150, marginBottom: 150, marginRight: 700}}/>
       </div> 
-      <div>
+      <div className='preview'>
         <h1>Hello, I'm Yakup KOK!</h1>
         <p>I'm a focused on creating creative and innovative solutions</p>
         <p> I'm delighted to have you here!</p>
