30bece34397b77ed45f7dd2ac3c3128479c8b8af
Yakup KÖK <yakupkok@172-2-0-135.lightspeed.dybhfl.sbcglobal.net>
2023-12-23 15:28:31 +0300

    contact page add and footer icon changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,17 @@ import '../styles/Footer.css'
 import { Link } from 'react-router-dom'
 
 //icon 
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
+import { FaMedium } from "react-icons/fa6";
+import { BsStackOverflow } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+
+
+
+
 
 
 function Footer() {
@@ -19,41 +25,48 @@ function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <GitHubIcon />
+          <FaGithub />
         </a>
         <a
           href="https://www.linkedin.com/in/yakupkok/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <LinkedInIcon />
+          <FaLinkedin />
         </a>
         <a
           href="https://stackoverflow.com/users/22748871/yakup-kok"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <StackedLineChartIcon />
+          <BsStackOverflow />
+        </a>
+        <a
+          href="https://medium.com/@yakupcia"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaMedium />
         </a>
         <a
           href="https://instagram.com/yakupcia"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <InstagramIcon />
+          <FaInstagramSquare />
         </a>
         <a
           href="https://twitter.com/yakupcia"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <TwitterIcon />
+          <FaTwitter />
         </a>
       </div>
       <div className='innerFooter'>
         <div className='footerLinkContainer'>
-        <Link to="/blog">Blog</Link>
-        <Link to="/projects">Projects</Link>
+        <Link to="/blog">Home</Link>
+        <Link to="/projects">About</Link>
         <Link to="/contact">Contact</Link>
         </div>
         <div className='footerWriterContainer'>
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,14 +1,65 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+
+import { IoIosSend } from "react-icons/io";
+
+
 function Contact() {
   return (
     <div>
       <Helmet>
         <title>Contact - Yakup KOK</title>
       </Helmet>
+      <section className="contact">
+        <h2>Get In <span>Touch</span> </h2>
 
-    <p> Contact</p>    
+        <div className='contact_info'>
+          <div>
+            <EmailIcon />
+
+          <span className="info_title">Mail Me</span>
+          <span className="info_disc">yakupcia@gmail.com</span>
+          </div>
+
+          <div>
+            <CallIcon />
+
+          <span className="info_title">Call Me</span>
+          <span className="info_disc">+90 555 024 39 10</span>
+          </div>
+
+          <form className='contact_form'>
+          <div className='form_input-group'>
+            <div className='form_input-div'>
+              <input type="text" placeholder='Your Name' className='form_kontrol'/>
+            </div>
+
+            <div className='form_input-div'>
+              <input type="email" placeholder='Your Email' className='form_kontrol'/>
+            </div>
+
+            <div className='form_input-div'>
+              <input type="text" placeholder='Your Subject' className='form_kontrol'/>
+            </div>
+
+            <div className='form_input-div'>
+             <textarea name="" id="" cols="30" rows="10" placeholder='Your Message' className='form_input textarea'></textarea>
+            </div>
+
+            <button className='Button'> 
+              Send Message 
+              <span className='send_button_icon'>
+              <IoIosSend />
+              </span>
+            </button>
+          </div>
+       </form>
+
+       </div>
+      </section>
     </div>
   )
 }
