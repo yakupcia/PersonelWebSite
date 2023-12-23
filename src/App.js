430ca9fd27788cb21f799647ad515d8bf043bd6f
Yakup KÖK <yakupkok@172-2-0-135.lightspeed.dybhfl.sbcglobal.net>
2023-12-23 14:18:33 +0300

    .

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
       <AnimCursor />
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
